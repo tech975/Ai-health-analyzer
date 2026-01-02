3c53932e27e7222fa3e46d21893b1939e3c1c08f
tech975 <tech@colabplatforms.com>
2026-01-02 18:40:49 +0530

    testing

--- client/src/pages/ReportPage.tsx
+++ client/src/pages/ReportPage.tsx
@@ -148,7 +148,7 @@ const ReportPage = () => {
             onClick={() => navigate('/')}
             className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors"
           >
-            Upload New Report
+            Upload New Report...
           </button>
         </div>
       </div>
